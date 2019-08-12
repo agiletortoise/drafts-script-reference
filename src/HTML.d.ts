6601b607f799de668e3377c57d9d857dd0c0e47e
Greg Pierce <greg@agiletortoise.com>
2019-08-12 12:26:13 -0500

    new HTML object

---
+++ src/HTML.d.ts
@@ -0,0 +1,28 @@
+/**
+ * # HTML
+ * 
+ * Helper methods to escape and unescape HTML entities in a string.
+ * 
+ * _Added: R15_
+ * 
+ *  ### Examples
+ * 
+ * ```javascript
+ * let s = "One & Two";
+ * let escaped = HTML.escape(s); // "One &#x26; Two"
+ * let unescaped = HTML.unescape(encoded); // "One & Two"
+ * ```
+ */
+declare class HTML {
+    /**
+     * Escape HTML entities in a string to be HTML safe.
+     * @param string the string to escape
+     */
+    static escape(string: string): string
+
+    /**
+     * Unescape HTML entities in a string.
+     * @param string the string to unescape
+     */
+    static unescape(string: string): string
+}
