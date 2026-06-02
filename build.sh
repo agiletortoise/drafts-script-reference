2321d62ec341b0b6458c594a82ad4615d417d026
Greg Pierce <greg@agiletortoise.com>
2026-06-02 16:05:44 -0500

    change theme

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ echo "Clobber old files..."
 rm -rf docs
 
 echo "Compile SASS..."
-npx sass themes/default/assets/css/main.sass themes/default/assets/css/main.css
+#npx sass themes/default/assets/css/main.sass themes/default/assets/css/main.css
 
 echo "Combine Sources..."
 rm -rf src/drafts.d.ts
 cat src-individual/*.d.ts >> src/drafts.d.ts
 
 echo "Run TypeDoc..."
-#npx typedoc --mode file --out "./docs" --name "Drafts Script Reference" src --includeDeclarations --theme "./themes/default" --excludeExternals --tsconfig "tsconfig.json"
-npx typedoc
+#npx typedoc --mode file --out "./docs" --name "Drafts Script Reference" src --includeDeclarations --plugin typedoc-theme-fresh --theme fresh --excludeExternals --tsconfig "tsconfig.json"
+npx typedoc --plugin typedoc-theme-fresh --theme fresh
 
 echo "Create drafts-definitions.js"
 cat src-individual/* >> docs/drafts-definitions.js
